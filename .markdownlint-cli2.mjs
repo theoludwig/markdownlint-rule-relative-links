70bdb7013eacba690a64bee78a5789aa3781aabe
Théo LUDWIG <contact@theoludwig.fr>
2024-12-28 23:15:32 +0100

    style: fix prettier

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -2,7 +2,7 @@ import relativeLinksRule from "./src/index.js"
 
 const config = {
   config: {
-    "extends": "markdownlint/style/prettier",
+    extends: "markdownlint/style/prettier",
     default: true,
     "relative-links": true,
     "no-inline-html": false,
